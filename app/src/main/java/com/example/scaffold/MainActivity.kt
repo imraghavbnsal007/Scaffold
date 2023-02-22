8e980a6f41ce95048a4b7cf94d7954d7ad4ebd98
imraghavbnsal007 <raghavb025@gmail.com>
2023-02-22 12:14:07 +0200

    Scaffold updated

--- app/src/main/java/com/example/scaffold/MainActivity.kt
+++ app/src/main/java/com/example/scaffold/MainActivity.kt
@@ -7,12 +7,16 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.scaffold.ui.theme.ScaffoldTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,18 +31,11 @@ class MainActivity : ComponentActivity() {
     }
 }
 @Composable
-fun MyTopBar()
+fun MainTopBar(title: String, navController: NavController)
 {
     var expanded by remember { mutableStateOf(false) }
     TopAppBar(
-        title = { Text(text = "My App")},
-        navigationIcon = {
-            IconButton(
-                onClick = { /*TODO*/ }
-            ) {
-                Icon(Icons.Filled.Menu, contentDescription = null)
-            }
-        },
+        title = { Text(title)},
         actions = {
             IconButton(
                 onClick = {
@@ -51,10 +48,12 @@ fun MyTopBar()
             DropdownMenu(
                 expanded = expanded,
                 onDismissRequest = { expanded = false }) {
-                DropdownMenuItem(onClick = { /*TODO*/ }) {
+                DropdownMenuItem(onClick = { 
+                    navController.navigate("info")
+                }) {
                     Text(text = "Info")
                 }
-                DropdownMenuItem(onClick = { /*TODO*/ }) {
+                DropdownMenuItem(onClick = { navController.navigate("settings")}) {
                     Text(text = "Settings")
                 }
 
@@ -62,18 +61,69 @@ fun MyTopBar()
         }
     )
 }
+@Composable
+fun ScreenTopBar(title: String, navController: NavController) {
+    TopAppBar(
+        title = { Text(title)},
+        navigationIcon = {
+            IconButton(onClick = { navController.navigateUp() }) {
+                Icon(Icons.Filled.ArrowBack, contentDescription = null)
+                
+            }
+        }
+    )
+        
+
+}
+
+@Composable
+fun MainScreen(navController: NavController)
+{
+    Scaffold(
+        topBar = { MainTopBar(title = "My App", navController)},
+        content = { Text(text = "Content for Home Screen")},
+    )
+}
+@Composable
+fun InfoScreen(navController: NavController)
+{
+    Scaffold(
+        topBar = { ScreenTopBar(title = "Info", navController)},
+        content = { Text(text = "Content for Info Screen")},
+    )
+}
+@Composable
+fun SettingsScreen(navController: NavController)
+{
+    Scaffold(
+        topBar = { ScreenTopBar(title = "Settings", navController)},
+        content = { Text(text = "Content for Settings Screen")},
+    )
+}
+
+
 
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun ScaffoldApp()
 {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "Home")
+    {
+        composable(route = "Home") {
+            MainScreen(navController)
+        }
+        composable(route = "Info") {
+            InfoScreen(navController)
+        }
+        composable(route = "Settings") {
+            SettingsScreen(navController)
+        }
 
-    Scaffold(
-        topBar = { MyTopBar() },
-        content = { Text(text = "Content for Home screen")},
-//        bottomBar = { BottomAppBar { Text(text = "Bottom Bar") }}
-    )
+
+
+    }
 }
 
 
